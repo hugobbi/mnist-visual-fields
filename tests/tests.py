dbde7b22ccd18384b8a9b854f4ee2fdc66b62da0
hugobbi <hgugobbi@gmail.com>
2024-06-17 20:04:42 -0300

    sync: attempting to multiprocess prototype generation

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,24 @@
+import multiprocessing as mp
 import numpy as np
 
-a = [np.array([8, 9, 10, 11]), np.array([12, 13, 14])]
-b = [np.array([1, 2, 3, 4]), np.array([5, 6, 7])]
+# test = np.array([
+#     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+#     [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+# ])
 
-c = np.mean([a, b], axis=0)
+# def process_element(i, j):
+#     print(test[i][j])
 
-print(c)
+# pool = mp.Pool()
+# for i in range(test.shape[0]):
+#     for j in range(test.shape[1]):
+#         pool.apply_async(process_element, args=(i, j))
 
+# pool.close()
+# pool.join()
+
+
+def tt(**a):
+    print(a)
+
+tt(a=1, b=2)
