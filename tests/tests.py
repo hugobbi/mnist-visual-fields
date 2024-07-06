0a7f7adefe3134a1629ddb6b530391a29b69736f
hugobbi <hgugobbi@gmail.com>
2024-07-06 13:22:45 -0300

    feat:  orthogonality calculation for DRFs and orth plotting

--- tests/tests.py
+++ tests/tests.py
@@ -1,24 +1,31 @@
-import multiprocessing as mp
 import numpy as np
 
-# test = np.array([
-#     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-#     [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-# ])
+def get_quadrants(matrix: np.array):
+    """
+    Returns the four quadrants of a matrix
 
-# def process_element(i, j):
-#     print(test[i][j])
+    Input:
+    matrix: np.array: matrix to be split into quadrants
 
-# pool = mp.Pool()
-# for i in range(test.shape[0]):
-#     for j in range(test.shape[1]):
-#         pool.apply_async(process_element, args=(i, j))
+    Output:
+    Tuple[np.array, np.array, np.array, np.array]: tuple containing the four quadrants of the matrix
+    """
 
-# pool.close()
-# pool.join()
+    n = matrix.shape[0]
+    half_n = n // 2
+    upper_left = matrix[:half_n, :half_n]
+    upper_right = matrix[:half_n, half_n:]
+    lower_left = matrix[half_n:, :half_n]
+    lower_right = matrix[half_n:, half_n:]
 
+    return upper_left, upper_right, lower_left, lower_right
 
-def tt(**a):
-    print(a)
+a = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
+print(a)
 
-tt(a=1, b=2)
+upper_left, upper_right, lower_left, lower_right = get_quadrants(a)
+
+print(upper_left)
+print(upper_right)
+print(lower_left)
+print(lower_right)
