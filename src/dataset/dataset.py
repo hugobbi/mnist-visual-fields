cec8aad278b4e303179b0d3f870d58d8d79ae2ca
hugobbi <hgugobbi@gmail.com>
2024-09-16 10:59:32 -0300

    annotating

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -14,13 +14,13 @@ class DataTwoFields:
     y: np.ndarray
 
 class Dataset:
-    def __init__(self, x_train, x_test, y_train, y_test):
+    def __init__(self, x_train: np.array, x_test: np.array, y_train: np.array, y_test: np.array):
         self.train = Data(x_train, y_train) 
         self.test = Data(x_test, y_test)
         self.train_vf = DataTwoFields(None, None, None)
         self.test_vf = DataTwoFields(None, None, None)
 
-    def shuffle(self, data, n: int | None = None):
+    def shuffle(self, data: Data, n: int | None = None):
         original_size = len(data.y)
         if n is None:
             random_indices = np.random.permutation(original_size)
@@ -29,7 +29,7 @@ class Dataset:
 
         return data.x[random_indices], data.y[random_indices]
 
-    def shuffle_vf(self, vf, n: int | None = None):
+    def shuffle_vf(self, vf: DataTwoFields, n: int | None = None):
         original_size = len(vf.y)
         if n is None:
             random_indices = np.random.permutation(original_size)
@@ -37,18 +37,6 @@ class Dataset:
             random_indices = np.random.choice(np.arange(0, original_size), n)
 
         return vf.x_left[random_indices], vf.x_right[random_indices], vf.y[random_indices]
-    
-    def shuffle_train(self, n: int | None = None):
-        self.train.x, self.train.y = self.shuffle(self.train, n)
-    
-    def shuffle_test(self, n: int | None = None):
-        self.test.x, self.test.y = self.shuffle(self.test, n)
-
-    def shuffle_train_vf(self, n: int | None = None):
-        self.train_vf.x_left, self.train_vf.x_right, self.train_vf.y = self.shuffle_vf(self.train_vf, n)
-    
-    def shuffle_test_vf(self, n: int | None = None):
-        self.test_vf.x_left, self.test_vf.x_right, self.test_vf.y = self.shuffle_vf(self.test_vf, n)
 
     def build_vf(self, x_data: np.array, y_data: np.array) -> Tuple[np.array, np.array, np.array]:
         n = len(y_data) * self.final_size
@@ -96,14 +84,12 @@ class Dataset:
         Builds an entire double visual fields dataset, comprised of two visual fields, left and right, and an array of the corresponding label for both visual fields.
 
         Input:
-        x_data: np.array(np.ndarray): array of two-dimensional arrays corresponding to the pixel values of digits of the MNIST dataset.
-        y_data: np.array(int): corresponding value of the digit represented by x_data.
-        final_size: float: how many times the final dataset is bigger than the input data. Default is 1.
-        proportion_cs: float: proportion of entries in the final dataset that have CS over SS. Default is 0.5.
-        proportion_left: float: proportion of entries in the final dataset that have attention on the left visual field. Default is 0.5.
-        full_attention_value: float: value of the full attention in CS. Default is 1.
-        reduced_attention_value: float: value of the reduced attention in CS. Default is 0.5.
-        ss_attention_value: float: value of the attention for SS. Default is 0.5.
+        final_size: float: how many times the final dataset is bigger than the input data (default: 1)
+        proportion_cs: float: proportion of entries in the final dataset that have CS over SS (default is 0.5)
+        proportion_left: float: proportion of entries in the final dataset that have attention on the left visual field (default is 0.5)
+        full_attention_value: float: value of the full attention in CS (default is 1)
+        reduced_attention_value: float: value of the reduced attention in CS (default is 0.5)
+        ss_attention_value: float: value of the attention for SS (default is 0.5)
         """
         
         self.final_size = final_size
