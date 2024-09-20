7bf60b7d2bb58b3d7b169c419a275daa669ba2be
hugobbi <hgugobbi@gmail.com>
2024-09-19 21:26:27 -0300

    updating setup file
    Former-commit-id: 5d4c882dc41f309bc7478476bacd55b6080263e1
    Former-commit-id: e44a3c6d6348816958d64266980aecb4f960a01f

--- setup.py
+++ setup.py
@@ -2,7 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='mnist-visual-fields',
+    author='Henrique Uhlmann Gobbi',
+    author_email='hgugobbi@gmail.com',
     version='1.0',
     packages=find_packages(),
+    install_requires=[
+        'tensorflow==2.15.0',
+        'numpy',
+        'matplotlib',
+        'attr',
+        'ipywidgets'
+    ],
+    url='https://github.com/hugobbi/mnist-visual-fields',
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    description='Code to handle creation and visualization of neural networks using Keras. It was made to work with the MNIST dataset, and the neural networks can have one or two visual fields (inputs).'
 )
-
