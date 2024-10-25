60ef883b0efe6d2ec713b0227f990a390d5d663c
hugobbi <hgugobbi@gmail.com>
2024-10-25 00:31:27 -0300

    fixing bug in svf plotting
    Former-commit-id: 0fb644a8c94adcace84a43cdeaa6a528f2b0b7e7
    Former-commit-id: 5121f71c5ac8f21e665d9c84bb1b8e4e302e649e
    Former-commit-id: 08773294dbada99ef565785e2dc3cfea5f2848d8

--- modules/plotting_network/plotting_network.py
+++ modules/plotting_network/plotting_network.py
@@ -89,6 +89,8 @@ class PlottingControl:
 
     plotted: List[PlottedLayer] = []
     is_left_vf: bool = False
+    is_right_vf: bool = False
+    is_center_vf: bool = False
     has_concatenated: bool = False
     vf_top: float = 0
     vf_bottom: float = 0
@@ -127,9 +129,12 @@ class NeuralNetworkPlotter:
         self.save_plots = save_plots
         self.__controller = None
 
-    def plot(self, 
-             left_vf_data: np.array, 
-             right_vf_data: np.array,
+    @property
+    def is_dvf_plot(self) -> bool:
+        return is_dvf_model(self.model)
+    
+    def plot(self,
+             *input_data: np.array,
              model: Optional[tf.keras.Model] = None,
              max_neurons: Optional[int] = None,
              weight_threshold: Optional[float] = None,
@@ -138,15 +143,12 @@ class NeuralNetworkPlotter:
              save_plot: Optional[bool] = None) -> None:
         
         """
-        Receives a trained model with two visual fields and an input, displaying the entire neural network with its
+        Receives a trained model and an input, displaying the entire neural network with its
         activations for that input.
-
-        Observation: the attention value for each visual field is applied in the function input data
 
         Input: 
         model: tf.keras.Model: model to be used for visualization
-        left_vf_data: np.array: left visual field data
-        right_vf_data: np.array: right visual field data
+        input_data: np.array: input data to be used for visualization
         max_neurons: int: maximum number of neurons to be plotted in a layer (default: 300)
         weight_threshold: float: minimum weight value to be plotted, considering normalized values between 0 and 1 (default: 0.5)
         attribute_lenses: Optional[List[tf.keras.Model]]: list of models to be used as attribute lenses for each layer (default: None)
@@ -158,18 +160,18 @@ class NeuralNetworkPlotter:
         """
         
         # Setting default values
-        if model is None:
-            model = self.model
-        if max_neurons is None:
-            max_neurons = self.max_neurons
-        if weight_threshold is None:
-            weight_threshold = self.weight_threshold
-        if attribute_lenses is None:
-            attribute_lenses = self.attribute_lenses
-        if num_attr_lenses_top_activations is None:
-            num_attr_lenses_top_activations = self.num_attr_lenses_top_activations
-        if save_plot is None:
-            save_plot = self.save_plots
+        if model is not None:
+            self.model = model
+        if max_neurons is not None:
+            self.max_neurons = max_neurons 
+        if weight_threshold is not None:
+            self.weight_threshold = weight_threshold
+        if attribute_lenses is not None:
+            self.attribute_lenses = attribute_lenses
+        if num_attr_lenses_top_activations is not None:
+            self.num_attr_lenses_top_activations = num_attr_lenses_top_activations
+        if save_plot is not None:
+            self.save_plots = save_plot
 
         # Determining matplotlib figure parameters
         fig, ax = plt.subplots(figsize=(32, 32))
@@ -178,10 +180,16 @@ class NeuralNetworkPlotter:
         middle = round((top + bottom) / 2, ndigits=4)
         MIDDLE_SPACING = 0.02
 
-        # Calculating neural network activations
-        model_activations = compute_activations(model, left_vf_data, right_vf_data)
+        # Calculating activations
+        if self.is_dvf_plot:
+            left_vf_data, right_vf_data = input_data
+            model_activations = compute_activations(self.model, left_vf_data, right_vf_data)
+        else:
+            input_data = input_data[0]
+            model_activations = compute_activations(self.model, input_data)
 
         # Determining neural network figure parameters
+        TOTAL_LAYER_SPACES = self.__compute_total_layer_spaces()
         CONECTION_OPACITY = 0.2
         COLOR_CONECTION_OPACITY = 0.5
         IMAGE_OFFSET = 0.024
@@ -191,26 +199,36 @@ class NeuralNetworkPlotter:
         RIGHT_VF_MIDDLE = 0.5 * middle
         INITIAL_LEFT_VF_POSITION = Position(left, 1.5 * middle) 
         INITIAL_RIGHT_VF_POSITION = Position(left, 0.5 * middle)  
-        TOTAL_LAYER_SPACES = self.__compute_total_layer_spaces()
+        INITIAL_CENTER_VF_POSITION = Position(left, middle)  
+        AL_TEXT_X_OFFSET = -0.02
+        AL_TEXT_Y_OFFSET = 0.0325
 
         # Setting up control structure to plot layers
         self.__controller = PlottingControl()
-        for i, (layer, activations) in enumerate(zip(model.layers, model_activations)):
+        for i, (layer, activations) in enumerate(zip(self.model.layers, model_activations)):
+            # if 5 <= i <= 6: continue
+            print(f"Verificando camada {layer.name}")
             plotted_layer = PlottedLayer(original_layer=layer, activations=activations)
             if plotted_layer.type not in self.__controller.SHOULD_PLOT: continue # only plots layers that should be plotted
             # Determining layer characteristics
-            self.__controller.is_left_vf = i % 2 == 0 and not (plotted_layer.is_concatenate_layer or self.__controller.has_concatenated)
+            self.__controller.is_center_vf = plotted_layer.is_concatenate_layer or self.__controller.has_concatenated or not is_dvf_plot
+            self.__controller.is_left_vf = not self.__controller.is_center_vf and i % 2 == 0
+            self.__controller.is_right_vf = not self.__controller.is_center_vf and i % 2 != 0 
             self.__controller.vf_top = top
             self.__controller.vf_bottom = bottom
             # Left VF
             if self.__controller.is_left_vf:
                 self.__controller.vf_bottom = middle + MIDDLE_SPACING
             # Right VF
-            elif not (plotted_layer.is_concatenate_layer or self.__controller.has_concatenated):
+            if self.__controller.is_right_vf:
                 self.__controller.vf_top = middle - MIDDLE_SPACING
             # Input
             if plotted_layer.is_input_layer:
-                plotted_layer.position = INITIAL_LEFT_VF_POSITION if self.__controller.is_left_vf else INITIAL_RIGHT_VF_POSITION
+                print(f"Input {layer.name}")
+                if is_dvf_plot:
+                    plotted_layer.position = INITIAL_LEFT_VF_POSITION if self.__controller.is_left_vf else INITIAL_RIGHT_VF_POSITION
+                else:
+                    plotted_layer.position = INITIAL_CENTER_VF_POSITION
                 ab = self.__generate_image_annotation_box(
                             plotted_layer.activations,
                             plotted_layer.position,
@@ -231,20 +249,24 @@ class NeuralNetworkPlotter:
                 self.__controller.has_concatenated = True
             # Output
             if plotted_layer.is_output_layer:
+                print("OUTPUT")
                 OUTPUT_OFFSET = 0.2
                 self.__controller.vf_top = middle + OUTPUT_OFFSET
                 self.__controller.vf_bottom = middle - OUTPUT_OFFSET
                         
             # Getting previously plotted layer
             previous_layer = plotted_layer.get_previously_plotted_layer(self.__controller.plotted)
-            should_plot_weights = plotted_layer.num_neurons <= max_neurons and previous_layer.num_neurons <= max_neurons and not plotted_layer.is_concatenate_layer
+            should_plot_weights = plotted_layer.num_neurons <= self.max_neurons and previous_layer.num_neurons <= self.max_neurons and not plotted_layer.is_concatenate_layer
             
             # Determining layer spacing and layer position
+            print(TOTAL_LAYER_SPACES)
             layer_spacing = (right - left - TEXT_X_OFFSET) / TOTAL_LAYER_SPACES if should_plot_weights else LAYER_SPACING_NO_WEIGHTS
+            layer_spacing = LAYER_SPACING_NO_WEIGHTS
             plotted_layer.position.x = previous_layer.position.x + layer_spacing
 
             # Plotting neurons and weights
-            if plotted_layer.num_neurons <= max_neurons:
+            if plotted_layer.num_neurons <= self.max_neurons:
+                print(f"Ploting {layer.name}: {i=}")
                 # Plotting neurons
                 plotted_layer.position.y = self.__controller.vf_top
                 neuron_spacing = (self.__controller.vf_top - self.__controller.vf_bottom) / plotted_layer.num_neurons
@@ -268,7 +290,7 @@ class NeuralNetworkPlotter:
                     if should_plot_weights:
                         layer_weights_neuron = normalize_ndarray(plotted_layer.weights[:, j])
                         for previous_neuron, connection_weight in zip(previous_layer.neurons, layer_weights_neuron):
-                            if connection_weight < weight_threshold: continue
+                            if connection_weight < self.weight_threshold: continue
                             connection = plt.Line2D(
                                 [neuron.position.x - neuron.radius, 
                                  previous_neuron.position.x + previous_neuron.radius],
@@ -302,15 +324,16 @@ class NeuralNetworkPlotter:
                         ax.add_artist(connection)
             else:
                 # Plotting layer as image
+                activations_image = self.__get_image(plotted_layer.activations)
                 ab = self.__generate_image_annotation_box(
-                            plotted_layer.activations,
+                            activations_image,
                             plotted_layer.position,
                             cmap="binary",
                             size=self.__calculate_image_size(plotted_layer.num_neurons),
                         )
                 ax.add_artist(ab)
                 # Plotting symbolic connections from image to previous layer
-                if previous_layer.num_neurons <= max_neurons:
+                if previous_layer.num_neurons <= self.max_neurons:
                     for previous_neuron in previous_layer.neurons:
                         connection = plt.Line2D(
                             [plotted_layer.position.x - IMAGE_OFFSET, 
@@ -333,28 +356,29 @@ class NeuralNetworkPlotter:
                         ax.add_artist(connection)
             
             # Plotting attribute lenses
-            if attribute_lenses is not None:
-                al_idx = len(self.__controller.plotted) - len(model.input) # attribute lens index will be all plotted layers except input layers
-                if al_idx < len(attribute_lenses):
+            if self.attribute_lenses is not None:
+                al_idx = len(self.__controller.plotted) - (len(self.model.input) if self.is_dvf_plot else 1) # attribute lens index will be all plotted layers except input layers
+                if al_idx < len(self.attribute_lenses):
                     attribute_lens = self.attribute_lenses[al_idx]
-                    if is_dvf_model(attribute_lens):
-                        digit_activations = compute_digits_model_predicts(attribute_lens, left_vf_data, right_vf_data)
-                    else:
-                        if self.__controller.is_left_vf:
-                            digit_activations = compute_digits_model_predicts(attribute_lens, left_vf_data)
+                    if self.is_dvf_plot:
+                        if is_dvf_model(attribute_lens):
+                            digit_activations = compute_digits_model_predicts(attribute_lens, left_vf_data, right_vf_data)
                         else:
-                            digit_activations = compute_digits_model_predicts(attribute_lens, right_vf_data)
+                            if self.__controller.is_left_vf:
+                                digit_activations = compute_digits_model_predicts(attribute_lens, left_vf_data)
+                            else:
+                                digit_activations = compute_digits_model_predicts(attribute_lens, right_vf_data)
+                    else:
+                        digit_activations = compute_digits_model_predicts(attribute_lens, input_data)
                     
                     al_text_string = ''
-                    for k in range(num_attr_lenses_top_activations):
-                        eol = '\n' if k != num_attr_lenses_top_activations - 1 else ''
+                    for k in range(self.num_attr_lenses_top_activations):
+                        eol = '\n' if k != self.num_attr_lenses_top_activations - 1 else ''
                         al_text_string += f'{digit_activations[k][0]}: {digit_activations[k][1]:.4f}{eol}'
-                    if plotted_layer.num_neurons <= max_neurons:
+                    if plotted_layer.num_neurons <= self.max_neurons:
                         al_text_position = plotted_layer.neurons[-1].position.copy()
                     else:
                         al_text_position = Position(plotted_layer.position.x, plotted_layer.position.y - IMAGE_OFFSET)
-                    AL_TEXT_X_OFFSET = -0.02
-                    AL_TEXT_Y_OFFSET = 0.0325
                     text = plt.Text(
                         al_text_position.x + AL_TEXT_X_OFFSET,
                         al_text_position.y - AL_TEXT_Y_OFFSET,
@@ -369,7 +393,7 @@ class NeuralNetworkPlotter:
             self.__controller.plotted.append(plotted_layer)
         
         # Saving plot
-        if save_plot:
+        if self.save_plot:
             save_dir = "results/images/"
             os.makedirs(save_dir, exist_ok=True)
             plt.savefig(f"{save_dir}/NN_PLOT_{get_current_time_string()}.png")  
@@ -430,7 +454,7 @@ class NeuralNetworkPlotter:
         b = 533/96
         return number_neurons * a + b
     
-    def __compute_total_layer_spaces(self) -> int:
+    def compute_total_layer_spaces(self) -> int:
         """
         Computes the total number of layer spaces a neural network plot will have
         """
@@ -439,40 +463,29 @@ class NeuralNetworkPlotter:
         for layer in self.model.layers:
             if layer.__class__.__name__ not in PlottingControl.SHOULD_PLOT: continue    
             i += 1
-            if not has_concatenated:
+            if not has_concatenated and not self.is_dvf_plot:
                 j += 1
                 if layer.__class__.__name__ == "Concatenate": has_concatenated = True
-        
-        return int(i - j // 2) - 1
 
-    # def __get_attribute_lens(self, plotted_layer: PlottedLayer) -> tf.keras.Model:
-    #     """
-    #     Matches second to last layer of attribute lense to plotted layer name in order
-    #     to find its corresponing attribute lens model
-    #     """
-    #     for al in self.attribute_lenses:
-    #         if al.layers[-2].name == plotted_layer.name:
-    #             return al
-    #     raise ValueError(f"Attribute lens for layer '{plotted_layer.name}' not found")
+        return int(i - j // 2) - 1
     
-    def generate_attribute_lenses(self, model: Optional[tf.keras.Model] = None) -> List[tf.keras.Model]:
+    def generate_attribute_lenses(self, model: Optional[tf.keras.Model] = None) -> None:
         """
         Generates attribute lenses for each hidden layer of the model
         """
-        if model is None:
-            model = self.model
+        if model is not None:
+            self.model = model
         NOT_ATTRIBUTE_LENSES = {"InputLayer", "Flatten"}
         has_concatenated = False
-        is_svf = not is_dvf_model(model)
-        
+
         # Copying original model and freezing weigths
-        model_copy = tf.keras.models.clone_model(model)
-        model_weights = model.get_weights()
+        model_copy = tf.keras.models.clone_model(self.model)
+        model_weights = self.model.get_weights()
         model_copy.set_weights(model_weights)
         for layer in model_copy.layers: layer.trainable = False
 
         # Generating attribute lenses for each hidden layer
-        attribute_lenses = []
+        self.attribute_lenses = []
         for i, layer in enumerate(model_copy.layers[:-2]): # excluding output layer and last hidden layer
             # Layer type controller
             is_conv_layer = False
@@ -481,7 +494,7 @@ class NeuralNetworkPlotter:
             if layer.__class__.__name__ == "Concatenate": has_concatenated = True
             
             # Determining input layer
-            if has_concatenated or is_svf:
+            if has_concatenated or not self.is_dvf_plot:
                 input_layer = model_copy.input
             else:
                 input_layer = model_copy.input[0] if i % 2 == 0 else model_copy.input[1] # left or right input
@@ -497,10 +510,17 @@ class NeuralNetworkPlotter:
             attribute_lens = tf.keras.models.Model(
                 inputs=input_layer, outputs=output
             )
-            attribute_lenses.append(attribute_lens)
-        
-        self.attribute_lenses = attribute_lenses
-        return attribute_lenses
+            self.attribute_lenses.append(attribute_lens)
+
+    def __get_attribute_lens(self, plotted_layer: PlottedLayer) -> tf.keras.Model:
+        """
+        Matches second to last layer of attribute lense to plotted layer name in order
+        to find its corresponing attribute lens model
+        """
+        for al in self.attribute_lenses:
+            if al.layers[-2].name == plotted_layer.name:
+                return al
+        raise ValueError(f"Attribute lens for layer '{plotted_layer.name}' not found")
 
     def train_attribute_lenses(
             self, 
@@ -517,6 +537,8 @@ class NeuralNetworkPlotter:
             raise ValueError("Attribute lenses have not been generated yet")
         for al in self.attribute_lenses:
             if is_dvf_model(al):
+                n_train = len(dataset.train_vf.y)
+                n_test = len(dataset.test_vf.y)
                 training_generator = data_generator_dvf(
                     dataset.train_vf.x_left, dataset.train_vf.x_right, dataset.train_vf.y, batch_size
                 )
@@ -524,6 +546,8 @@ class NeuralNetworkPlotter:
                     dataset.test_vf.x_left, dataset.test_vf.x_right, dataset.test_vf.y, batch_size
                 )
             else:
+                n_train = len(dataset.train.y)
+                n_test = len(dataset.test.y)
                 training_generator = data_generator_svf(
                     dataset.train.x, dataset.train.y, batch_size
                 )
@@ -537,9 +561,9 @@ class NeuralNetworkPlotter:
             )
             al.fit(
                 training_generator,
-                steps_per_epoch=len(dataset.train_vf.y) // batch_size,
+                steps_per_epoch=n_train // batch_size,
                 epochs=epochs,
                 callbacks=[AccuracyLogging(self, al.name)],
                 validation_data=testing_generator,
-                validation_steps=len(dataset.test_vf.y) // batch_size
+                validation_steps=n_test // batch_size
             )
